7a92f69462481586ecd1d91dd3c8b9d2d424b72b
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2022-12-23 15:04:57 +0100

    removed detect-sources-in-dirty-image because seems unnecessary

--- karabo/sourcedetection/result.py
+++ karabo/sourcedetection/result.py
@@ -45,6 +45,7 @@ class SourceDetectionResult(KaraboResource):
         image: Image,
         beam: Optional[Tuple[float, float, float]] = None,
         quiet: bool = False,
+        format: str = 'csv',
         **kwargs,
     ) -> PyBDSFSourceDetectionResult:
         """
@@ -54,56 +55,24 @@ class SourceDetectionResult(KaraboResource):
         :param image: Image to perform source detection on.
         :param beam: FWHM of restoring beam. Specify as (maj, min. pos angle E of N). 
             None means it will try to be extracted from the Image data. (Might fail)
+        :param quiet: run function quietly?
+        :param format: output format
         :return: Source Detection Result containing the found sources
         """
+        if beam is None and SourceDetectionResult.image_has_beam_parameters(image):
+            beam = (
+                image.header["BMAJ"],
+                image.header["BMIN"],
+                image.header["BPA"]
+            )
+        else:
+            raise KaraboWarning("No beam parameter provided or found. Continue with `beam`=None !")
+
         detection = bdsf.process_image(
             image.file.path,
-            beam=beam,
-            quiet=quiet,
-            format="csv",
-            **kwargs,
-        )
-        
-        return PyBDSFSourceDetectionResult(detection)
-    
-    @staticmethod
-    def detect_sources_in_dirty_image(
-        imager: Imager,
-        dirty: Optional[Image] = None,
-        quiet: bool = False,
-        beam: Optional[Tuple[float, float, float]] = None,
-        beam_guessing_method: str = 'rascil_1_iter',
-        **kwargs,
-    ) -> PyBDSFSourceDetectionResult:
-        """
-        Detecting sources in an dirty image (No clean algorithm is applied). The Source detection is implemented with the PyBDSF.process_image function.
-        See https://www.astron.nl/citt/pybdsf/process_image.html for more information.
-
-        :param Imager: Imager. Because some information is needed from the imager class, don't pass directly the Image.
-        :param dirty: Image. Pass the precalculated dirty image to speedup the source detection.
-        :param beam: FWHM of restoring beam. Specify as (maj, min. pos angle E of N). 
-                        If not specified, the beam is read from the header or calculated with the help of rascil.
-        :return: Source Detection Result containing the found sources
-        """
-        if dirty is None:
-            dirty = imager.get_dirty_image()
-        if beam is None:
-            if SourceDetectionResult.image_has_beam_parameters(dirty):
-                beam = (
-                    dirty.header["BMAJ"],
-                    dirty.header["BMIN"],
-                    dirty.header["BPA"]
-                )
-            elif beam is None and not SourceDetectionResult.image_has_beam_parameters(dirty):
-                beam = SourceDetectionResult.guess_beam_parameters(imager, beam_guessing_method)
-            else:
-                raise KaraboWarning("No beam parameter found. Source detection might fail.")
-                         
-        detection = bdsf.process_image(
-            dirty.file.path,
-            beam=beam,
-            quiet=quiet,
-            format="csv",
+            beam = beam,
+            quiet = quiet,
+            format = format,
             **kwargs,
         )
         
@@ -216,7 +185,10 @@ class PyBDSFSourceDetectionResult(SourceDetectionResult):
         """
         sources_file = FileHandle()
         bdsf_detection.write_catalog(
-            outfile=sources_file.path, catalog_type="gaul", format="csv", clobber=True
+            outfile=sources_file.path,
+            catalog_type="gaul",
+            format="csv",
+            clobber=True,
         )
         bdsf_detected_sources = read_CSV_to_ndarray(sources_file.path)
 
